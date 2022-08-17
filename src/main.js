6277667fe1dbcc92e106f79697125f2669c06158
Aleja22LC <alejaleona@hotmail.com>
2022-08-17 18:17:58 -0500

    subiendo maquetado de nav y arreglos del fondo con css usando responsive

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import data from '/src/data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 console.log(data);
 //interaccion con el DOM
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle(".nav-menu_visible")
+    //condicion para buscar mas accesibilidad
+    if (navMenu.classList.contains("nav-menu_visible")){
+        navToggle.setAttribute("aria-label", "Cerrar Menú");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir Menú")
+    }
+});
+
