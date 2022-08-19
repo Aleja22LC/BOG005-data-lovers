d78ea7d64e7cdca3fb1e1b1a99da8d1d945d7354
Aleja22LC <alejaleona@hotmail.com>
2022-08-18 20:31:29 -0500

    subiendo boton hamburguesa funcionando y primera parte de la pag inicio

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 import data from '/src/data/athletes/athletes.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 console.log(data);
 //interaccion con el DOM
-const navToggle = document.querySelector(".nav-toggle")
-const navMenu = document.querySelector(".nav-menu")
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle(".nav-menu_visible")
+    navMenu.classList.toggle("nav-menu_visible")
     //condicion para buscar mas accesibilidad
     if (navMenu.classList.contains("nav-menu_visible")){
-        navToggle.setAttribute("aria-label", "Cerrar Menú");
+         navToggle.setAttribute("aria-label", "Cerrar Menú");
     } else {
-        navToggle.setAttribute("aria-label", "Abrir Menú")
+         navToggle.setAttribute("aria-label", "Abrir Menú")
     }
 });
+
+//crear funcion de crear tarjetas y luego crear una constante donde guardar la funcion
+
+// function crearTarjetas (atletas){
+//     const container =document.querySelector(".container");
+    
+// }
+
+
+//recorrer la data para obtener solo los paises
 
